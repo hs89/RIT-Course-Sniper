1d14154c4c5e5b47b32787419d5ef2ee42ec330d
hs89 <hsandlar@gmail.com>
2012-03-16 11:39:21 -0400

    Pulled out try-catch -- the bug is when the SIS system goes offline at 12:30 AM.. I will patch this tonight

--- sniper.rb
+++ sniper.rb
@@ -15,7 +15,6 @@ a = temp_user_input[0] #college number
 b = temp_user_input[1] #course number
 c = temp_user_input[2] #section number
 
-
 def registerCourse(course_number)
   agent = Mechanize.new
   agent.user_agent_alias = 'Windows Mozilla'
@@ -42,53 +41,45 @@ def registerCourse(course_number)
   end  
 end
 
-begin
-  loop do
-    @count_attempts = @count_attempts + 1
-    agent = Mechanize.new
-    agent.user_agent_alias = 'Windows Mozilla'
-    agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    
-    #This line fools the PHP backend at RIT into thinking I actually viewed this page first
-    class_list = agent.get "https://sis.rit.edu/info/info.do?init=openCourses" 
-    
-    #Get the course details for the particular course number
-    course_details = agent.get "https://sis.rit.edu/info/courseDetail.do?init=openCourses&source=open&section=".concat(a+b+c)
-    
-    #Parse out how many spots available left in the course
-    course_details.body.match(/Current Enrollment:[^\d]*(\d+)/)
-    currently_enrolled = $1
-    course_details.body.match(/Max Enrollment:[^\d]*(\d+)/)
-    max_enrolled = $1
-    
-    #Report on what we found
-    puts "Course number: " + a+"-"+b+"-"+c
-    puts "Currently enrolled: " + currently_enrolled
-    puts "Max enrolled: " + max_enrolled
-    puts "Attempt: " + @count_attempts.to_s
-    puts "System time: " + Time.now.to_s  
-    
-=begin
-      Now that we know the number of students enrolled
-      and we know the max possible enrolled
-      determine if there is space in the class
-      and then if there is, log in to sis and
-      register the course
-=end
-    
-    
-    #If there is free space -> register; otherwise -> do nothing
-    if(currently_enrolled<max_enrolled)
-      if(registerCourse(a+b+c)) then exit
-      end
-    else
-      puts "Unable to register course ".concat(a+b+c+ " right now")
-      sleep 3  
-    end
-  end
+loop do
+  @count_attempts = @count_attempts + 1
+  agent = Mechanize.new
+  agent.user_agent_alias = 'Windows Mozilla'
+  agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
   
-rescue
-#don't do anything.. just let the loop go
+  #This line fools the PHP backend at RIT into thinking I actually viewed this page first
+  class_list = agent.get "https://sis.rit.edu/info/info.do?init=openCourses" 
+  
+  #Get the course details for the particular course number
+  course_details = agent.get "https://sis.rit.edu/info/courseDetail.do?init=openCourses&source=open&section=".concat(a+b+c)
+  
+  #Parse out how many spots available left in the course
+  course_details.body.match(/Current Enrollment:[^\d]*(\d+)/)
+  currently_enrolled = $1
+  course_details.body.match(/Max Enrollment:[^\d]*(\d+)/)
+  max_enrolled = $1
+  
+  #Report on what we found
+  puts "Course number: " + a+"-"+b+"-"+c
+  puts "Currently enrolled: " + currently_enrolled
+  puts "Max enrolled: " + max_enrolled
+  puts "Attempt: " + @count_attempts.to_s
+  puts "System time: " + Time.now.to_s  
+  
+=begin
+    Now that we know the number of students enrolled
+    and we know the max possible enrolled
+    determine if there is space in the class
+    and then if there is, log in to sis and
+    register the course
+=end
+  
+  #If there is free space -> register; otherwise -> do nothing
+  if(currently_enrolled<max_enrolled)
+    if(registerCourse(a+b+c)) then exit
+    end
+  else
+    puts "Unable to register course ".concat(a+b+c+ " right now")
+    sleep 3  
+  end
 end
-
-    
